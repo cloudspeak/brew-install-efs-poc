826d0cf85a1f89ad51cff6ca7a6f16de65da5e7e
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-07-14 13:59:42 +0000

    Created helper function for configuring Lambdas

--- __main__.py
+++ __main__.py
@@ -9,13 +9,12 @@ from pulumi.output import Output
 from typing import Dict
 from filebase64sha256 import filebase64sha256
 from pulumi_infrastructure.development_environment import DevelopmentEnvironment
+from pulumi_infrastructure.get_environment_function_args import get_environment_function_args
 
 environment = DevelopmentEnvironment("ExamplePOC",
   github_repo_name="https://github.com/cloudspeak/brew-install-efs-poc.git",
   github_version_name = "codebuild"
 )
-
-# IAM
 
 example_role = iam.Role("ExampleFunctionRole", assume_role_policy="""{
 "Version": "2012-10-17",
@@ -36,33 +35,14 @@ iam.RolePolicyAttachment("VpcAccessPolicyAttach",
     role=example_role.name
 )
 
-
-# Lambda
-
-mount_location = "/mnt/efs"
-
 example_function = lambda_.Function("exampleFunction",
         code="lambda.zip",
         source_code_hash=filebase64sha256("lambda.zip"),
         handler="handler.my_handler",
         role=example_role.arn,
         runtime="python3.8",
-        vpc_config={
-          "security_group_ids": [environment.security_group_id],
-          "subnet_ids": environment.public_subnet_ids
-        },
-        file_system_config={
-          "arn": environment.efs_access_point_arn,
-          "local_mount_path": mount_location
-        },
-        environment={
-          "variables": {
-            "LAMBDA_PACKAGES_PATH": mount_location,
-            "LD_LIBRARY_PATH": f"/var/lang/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib:/opt/lib:{mount_location}/lambda_packages/lib",
-            "PATH": f"/var/lang/bin:/usr/local/bin:/usr/bin/:/bin:/opt/bin:{mount_location}/lambda_packages/bin"
-          }
-        },
-        opts=ResourceOptions(depends_on=[environment])
+        opts=ResourceOptions(depends_on=[environment]),
+        **get_environment_function_args(environment)
 )
 
 pulumi.export('file_system_id', environment.file_system_id)
